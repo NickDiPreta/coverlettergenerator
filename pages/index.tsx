6a284a002fa2e40ce6f32f424ae509d6ff61c7b8
NickDiPreta <nick@getperch.app>
2023-03-16 20:19:34 -0400

    typo

--- pages/index.tsx
+++ pages/index.tsx
@@ -107,7 +107,7 @@ const Home: NextPage = () => {
             rows={4}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
             placeholder={
-              "e.g. Senior Developer Advocate @Vercel
+              "e.g. Senior Developer Advocate @Vercel"
             }
           />
 
